9368b141c15688806ee2bc406646492d6a20db1b
Caio Quinta <caiodaquinta@gmail.com>
2016-02-06 16:26:23 -0200

    form send personal fields and itens to back-end and trigger e-mail

--- Server.js
+++ Server.js
@@ -4,6 +4,13 @@ var app = express();
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
@@ -13,6 +20,12 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
+app.post('/sendorder', function (req, res) {
+  var name = req.body.name;
+  sendorder(req);
+  res.sendFile(path + "index.html");
+}); 
+
 app.use("/",router);
 app.use(express.static(__dirname + '/'));
 
@@ -20,26 +33,33 @@ app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
 
-//var nodemailer = require('nodemailer');
+sendorder = function(req) {
+  var nodemailer = require('nodemailer');
 
-// create reusable transporter object using the default SMTP transport
-//var transporter = nodemailer.createTransport('smtps://caiodaquinta@gmail.com:hagane22222@smtp.gmail.com');
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport('smtps://caiodaquinta@gmail.com:hagane22222@smtp.gmail.com');
 
-// setup e-mail data with unicode symbols
-/*var mailOptions = {
-    from: 'Caio <caiodaquinta@gmail.com>', // sender address
-    to: 'caiodaquinta@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world 🐴', // plaintext body
-    html: '<b>Hello world 🐴</b>' // html body
-};
-*/
-// send mail with defined transport object
-/*transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: 'Caio <caiodaquinta@gmail.com>', // sender address
+      to: req.body.email, // list of receivers
+      subject: 'Solicitação de Pedido ✔', // Subject line
+      text: 'Solicitação do Pedido', // plaintext body
+      html: '<h3>Dados do Cliente</h3><table><tr><td>Nome:</td><td>'+req.body.name+'</td></tr><tr><td>E-mail:</td><td>'+req.body.email+'</td></tr><tr><td>Endereço:</td><td>'+req.body.address+'</td></tr><td>Telefone:</td><td>'+req.body.phone+'</td></tr></table><h3>Resumo do pedido</h3>'+ orderItens(req) +'<img src='+ req.body.pier_image +'/>'//htmlbody
+    };
 
-*/
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+  });
+}
+
+orderItens = function(req) {
+  itens_list = '';
+  for (var i in req.body.itens) {
+    itens_list = itens_list + '<tr><td>'+req.body.itens[i]+'</td></tr>'
+  }
+  return itens_list
+}
