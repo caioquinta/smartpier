65256d55c7442e14f651c71abf3b447264875d9f
Caio Quinta <caiodaquinta@gmail.com>
2016-03-05 09:55:06 -0300

    rise express body-parser limite

--- Server.js
+++ Server.js
@@ -6,8 +6,9 @@ var order = require('./controllers/order_controller.js');
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use( bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use( bodyParser.urlencoded({
+	limit: '50mb',     // to support URL-encoded bodies
+  	extended: true
 })); 
 
 router.use(function (req,res,next) {
